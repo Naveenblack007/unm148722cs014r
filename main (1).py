0ab6ace39fbe5558dab72b583a76fcbb1bf11fd9
Naveenblack007 <144888972+Naveenblack007@users.noreply.github.com>
2023-10-28 11:36:16 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,14 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
-
-number= int(input("ENTER A VALUE:"))
-result= fact_rec(number)
-
-print("The Factorial of {} is {}.".format(number,result))
+#implement a class called player that represents a cricket player.the player class should have a method called play() which prints "the player is playing cricket".derive two classes, batsman and bowler, from the player class.override the play() method in each derived class to print "the batsman is batting "and "the bowler is bowling "respectively.write a program to create object of both the Batsman and bowler classes and call the play () method for each object
+class player:
+  def play (self):
+    print ("the player is playing cricket ")
+class Batsman(player):
+  def play (self):
+    print ("the batsman is batting  ")
+class Bowler(player):
+  def play (self):
+    print ("the bowler is bowling   ")
+batsman= Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-def isleapyear(year):
-  if(year % 4 == 0 and year % 100 != 0) or year % 400 ==0:
-    return True
+#implement a class called Bank account that represents a bank account.the class should have private attributes for a account number , account holder name and account balance include method to deposit money, withdraw money and display the account balance.ensure that the account balance that cannot be accessed directly from outside the class.write a program to create an instance of the bank account class and test the deposit and withdraw functionality 
+class bankaccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number =account_number
+    self.__account_holder_name =account_holder_name
+    self.__account_balance =initial_balance
+
+def deposit(self, amount):
+  if amount>0:
+    self. __account_balance +=amount
+    print("deposited ₹ {}.new balance:₹{}". format (amount,self.__account_balance))
   else:
-    return False
+    print ("invalid deposit amount.please deposit a positive amount")
+    def withdraw(self, amount ):
+      if amount >0 and amount <=self.__account_balance:
+        self.__account_balance -=amount
+        print ("withdraw ₹{}.new balance:₹{} ". format (amount,self.__account_balance ))
+      else:
+        print ("invalid withdrawal amount or insufficient balance")
+        def display_balance(self):
+          print("account balance for {}(account #{}):₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance ))
 
-year = int(input("Enter A Year:"))
-
-if isleapyear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+#create an instance of the Bank account class
+account=bankaccount(account_number ="123456789",account_holder_name="thirru", initial_balance=5000.0)
+#test deposit and withdraw functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw (200.0)
+account.display_balance()
